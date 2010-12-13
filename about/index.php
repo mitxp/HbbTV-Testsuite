308d15cae8a49cc97da2341a9a17f2e5c9b415d7
MIT-xperts GmbH <info@mit-xperts.com>
2010-12-13 21:43:29 +0100

    updated about page to include contributors and app source

--- about/index.php
+++ about/index.php
@@ -31,6 +31,10 @@ function handleKeyCode(kc) {
   return false;
 }
 function runStep(name) {
+  if (name=='contrib') {
+    document.getElementById('txtdiv').innerHTML = "Contributors to this testsuite are so far:<ul><li>Dora Babu<'+'/li><'+'/ul><br /><br />In case you have an additional test(s), we would be very happy to include your test in this testsuite. Please contact us (see cwleft sidee) to discuss further details.";
+    menuSelect(opts.length-1);
+  }
 }
 
 //]]>
@@ -43,28 +47,30 @@ function runStep(name) {
 <?php echo appmgrObject(); ?>
 
 <div class="txtdiv txtlg" style="left: 110px; top: 60px; width: 500px; height: 30px;">MIT-xperts HBBTV tests</div>
-<div class="txtdiv" style="left: 110px; top: 150px; width: 400px; height: 550px;"><u>Contact / Imprint:</u><br />
+<div class="txtdiv" style="left: 110px; top: 240px; width: 400px; height: 460px;"><u>Contact / Imprint:</u><br />
 MIT-xperts GmbH<br />
 Poccistr. 13<br />
 80336 Munich, Germany<br />
 info &#x40; mit-xperts&#x2e;com<br />
 Phone: +49 89 76756380<br /><br />
-<u>Contributing:</u><br />
-In case you have an additional test(s), we would be very happy to include your test in this testsuite. Please contact us (see above) to discuss further details.<br /><br />
+<u>This project is open source:</u><br />
+Contribute and share at<br />https://github.com/mitxp/HbbTV-Testsuite<br /><br />
+<u>Talk to us:</u><br />
+In case you think a test may need a fix, please contact us (or submit a fix yourself).
 </div>
 
-<div class="txtdiv" style="left: 700px; top: 110px; width: 400px; height: 600px;"><u>About this testsuite:</u><br />
+<div id="txtdiv" class="txtdiv" style="left: 700px; top: 110px; width: 400px; height: 600px;"><u>About this testsuite:</u><br />
 This test suite is for HbbTV terminal developers to test their implementation of the HbbTV 1.1.1 standard. Although this test suite contains a lot of test, it is not complete. It contains the most important interoperability issues disvocered in current applications. Tested parts are not covered by 100%, but the most importent checks are performed. Identified parts of the specification that are not covered so far are:<ul>
 <li>application/oipfDrmAgent (HbbTV 8.2.3)</li>
 <li>Parental access (OIPF DAE 4.5)</li>
 <li>Configuration class (OIPF DAE 7.3.2)</li>
-<li>AV components in video/broadcast object (OIPF DAE 7.13.4)</li>
 <li>JavaScript tests</li>
 <li>CSS navigation</li>
 </ul>
 </div>
 
 <ul id="menu" class="menu" style="left: 100px; top: 100px;">
+  <li name="contrib">Show contributors</li>
   <li name="exit">Return to test menu</li>
 </ul>
 
