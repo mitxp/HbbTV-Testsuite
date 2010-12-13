303bff4efad18879dd9356289b478a6d7328fae1
MIT-xperts GmbH <info@mit-xperts.com>
2010-12-13 13:51:26 +0100

    added testbox2 cert CN

--- clientssl/ssl/certcheck.php
+++ clientssl/ssl/certcheck.php
@@ -9,6 +9,7 @@ $ALLOWEDCN['icordhdplus.humaxdigital.com'] = 1;
 $ALLOWEDCN['icordhd-plus.humaxdigital.com'] = 1;
 $ALLOWEDCN['600s.videoweb.de'] = 1;
 $ALLOWEDCN['testbox.mit-xperts.com'] = 1;
+$ALLOWEDCN['testbox2.mit-xperts.com'] = 1;
 $ALLOWEDCN['volksbox.inverto.tv'] = 1;
 
 $msg = '';
