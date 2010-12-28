f4769548057c44b04ea53c5ab6adcaa27280500b
MIT-xperts GmbH <info@mit-xperts.com>
2010-12-28 10:59:11 +0100

    updated version

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-var releaseinfo = '1.3.1 (20101217)';
+var releaseinfo = '1.3.2 (20101228)';
 var service1 = [1,1068,5004,'430b011778000192a102750003'];
 var service2 = [1,1068,5008,service1[3]];
 var autostartappname = 'This testsuite application';
