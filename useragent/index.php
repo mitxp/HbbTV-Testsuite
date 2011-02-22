8983b5d3de75e79cdbabd508e22458fd71a5703a
MIT-xperts GmbH <info@mit-xperts.com>
2011-02-22 19:55:54 +0100

    separated browser and video player user agent check

--- useragent/index.php
+++ useragent/index.php
@@ -18,24 +18,14 @@ $videourl = 'http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']).'/ch
 ?>
 <script type="text/javascript">
 //<![CDATA[
+var vidto = false;
+
 window.onload = function() {
   menuInit();
   registerKeyEventListener();
   initApp();
-  var success = <?php echo $uagentok ? 'true' : 'false'; ?>;
-  showStatus(success, '<?php echo $uagentmsg; ?>');
-  if (success) {
-    setInstr('Browser user agent OK, checking video player user agent...');
-    var vid = document.getElementById('video');
-    vid.data = '<?php echo $videourl; ?>';
-    try {
-      vid.play(1);
-    } catch (e) {
-      // ignore
-    }
-    setTimeout(function() {checkPlayer();}, 5000);
-  }
-};
+  setInstr('Please run all steps in the displayed order. Navigate to the test using up/down, then press OK to start the test.');
+}
 function handleKeyCode(kc) {
   if (kc==VK_UP) {
     menuSelect(selected-1);
@@ -44,10 +34,50 @@ function handleKeyCode(kc) {
     menuSelect(selected+1);
     return true;
   } else if (kc==VK_ENTER) {
-    document.location.href = '../index.php';
+    var liid = opts[selected].getAttribute('name');
+    if (liid=='exit') {
+      document.location.href = '../index.php';
+    } else {
+      runStep(liid);
+    }
     return true;
   }
   return false;
+}
+function runStep(name) {
+  setInstr('Executing step...');
+  showStatus(true, '');
+  if (name=='browser') {
+    var success = <?php echo $uagentok ? 'true' : 'false'; ?>;
+    showStatus(success, '<?php echo $uagentmsg; ?>');
+  } else if (name=='vidplayer') {
+    if (vidto) {
+      clearTimeout(vidto);
+    }
+    setInstr('Starting video playback (waiting for onPlayStateChange event)...');
+    var vid = document.getElementById('video');
+    vid.onPlayStateChange = null;
+    try {
+      vid.stop();
+    } catch (e) {
+      // ignore
+    }
+    vidto = setTimeout(function() {vidto = false; checkPlayer();}, 30000);
+    vid.onPlayStateChange = function() {
+      if (6!=vid.playState && 5!=vid.playState) return;
+      if (vidto) {
+        clearTimeout(vidto);
+        vidto = false;
+      }
+      checkPlayer();
+    };
+    vid.data = '<?php echo $videourl; ?>';
+    try {
+      vid.play(1);
+    } catch (e) {
+      // ignore
+    }
+  }
 }
 function checkPlayer() {
   req = new XMLHttpRequest();
@@ -55,7 +85,7 @@ function checkPlayer() {
     if (req.readyState!=4 || req.status!=200) return;
     var s = req.responseText;
     if (s=='OK') {
-      showStatus(true, 'All user agents OK');
+      showStatus(true, 'Video player user agent OK');
     } else {
       showStatus(false, 'Video player user agent invalid: '+s);
     }
@@ -78,6 +108,8 @@ function checkPlayer() {
 <div class="txtdiv txtlg" style="left: 110px; top: 60px; width: 500px; height: 30px;">MIT-xperts HBBTV tests</div>
 <div id="instr" class="txtdiv" style="left: 700px; top: 110px; width: 400px; height: 360px;"></div>
 <ul id="menu" class="menu" style="left: 100px; top: 100px;">
+  <li name="browser">Test 1: Check browser user agent</li>
+  <li name="vidplayer">Test 2: Check video player user agent</li>
   <li name="exit">Return to test menu</li>
 </ul>
 <div id="status" style="left: 700px; top: 480px; width: 400px; height: 200px;"></div>
