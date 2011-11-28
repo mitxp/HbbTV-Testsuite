6748fc074a88ff3994fa698846a320397623acd1
MIT-xperts GmbH <info@mit-xperts.com>
2011-11-28 16:43:01 +0100

    added new cn

--- clientssl/ssl/certcheck.php
+++ clientssl/ssl/certcheck.php
@@ -13,6 +13,7 @@ $ALLOWEDCN['testbox.mit-xperts.com'] = 1;
 $ALLOWEDCN['testbox2.mit-xperts.com'] = 1;
 $ALLOWEDCN['volksbox.inverto.tv'] = 1;
 $ALLOWEDCN['viera2011.panasonic.com'] = 1;
+$ALLOWEDCN['viera2012.panasonic.com'] = 1;
 
 $msg = '';
 if (!$_SERVER['SSL_CLIENT_VERIFY']=='SUCCESS') {
