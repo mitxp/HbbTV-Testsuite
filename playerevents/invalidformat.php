b0a286f7c596ca725f2e17b3c7cc234a5d077f2c
MIT-xperts GmbH <info@mit-xperts.com>
2012-03-01 15:57:49 +0100

    changed invalid video format to a format closer to mp4 by request of samsung

--- playerevents/invalidformat.php
+++ playerevents/invalidformat.php
@@ -1,3 +1,4 @@
 <?php
 header('Content-Type: video/foobar');
-?>This is file has a video content type, but with a format that should not be supported by any player. Your player should report an error when playing this file.
+readfile('invalid.mp4');
+?>
