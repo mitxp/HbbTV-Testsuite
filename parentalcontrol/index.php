f19a28aaebf4b79abf55cde1e23152a8325ae421
MIT-xperts GmbH <info@mit-xperts.com>
2013-12-04 14:59:18 +0100

    accept any rating threshold

--- parentalcontrol/index.php
+++ parentalcontrol/index.php
@@ -97,11 +97,7 @@ function runStep(name) {
       showStatus(false, 'Current threshold rating name is not string representation of value attribute');
       return;
     }
-    if (rating.value!==3) {
-      showStatus(false, 'Current threshold rating value should be 3 (in dvb-si representing the age of 6 years), but returned value is '+rating.value+'.');
-      return;
-    }
-    showStatus(true, 'Current rating threshold is '+rating.value+', labels='+rating.labels+', region='+rating.region);
+    showStatus(true, 'Current rating threshold is '+rating.value+' (age '+(rating.value+3)+' years), labels='+rating.labels+', region='+rating.region);
   }
 }
 //]]>
